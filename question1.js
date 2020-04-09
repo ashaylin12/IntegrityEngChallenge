09801f5fce35fc902d9a21556b7cbcf8051b2606
ashaylin12 <ashaylin12@gmail.com>
2020-04-09 09:53:18 +0200

    Add files via upload
    These files run in Node.js . Simply load them in and they will run

---
+++ question1.js
@@ -0,0 +1,38 @@
+
+const readline = require('readline');
+const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    readlineInterface.question(questionText, resolve);
+  });
+}
+
+
+
+async function start() {
+  let arrnum = [null];
+  let size = await ask('Enter array size : ');
+
+  for(var i = 0 ; i<size; i++){
+
+     arrnum[i] = await ask('Enter a number : ');
+
+  }  
+  
+
+  function sortArray(arrNumbers){                                   
+                                      
+    arrNumbers.sort((a, b) => a - b);   
+                                       
+    return arrNumbers;   
+  }          
+
+
+  console.log('Sorted Array : ' + sortArray(arrnum) );
+ 
+}
+
+
+start();
+process.exit();
